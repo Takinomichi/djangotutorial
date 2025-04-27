e0df76f9370fd6894f12c00077095344d57d08ce
Takinomichi <okonomi.nyc@gmail.com>
2025-04-27 19:13:41 +0900

    added more testing methods and reverted back to function-based views

--- polls/views.py
+++ polls/views.py
@@ -2,60 +2,63 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views import generic
+from django.utils import timezone
+#from django.views import generic
 
 from .models import Choice, Question
 
-# These are the function-based views
-# That code loads the template called polls/index.html and passes it a context.
-# The context is a dictionary mapping template variable names to Python objects.
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     # Django provides a shortcut for rendering a template. Just use render() function instead of loader and HttpResponse
-#     # template = loader.get_template("polls/index.html")
-#     # return HttpResponse(template.render(context, request))
-#     return render(request, "polls/index.html", context)
-
-# def detail(request, question_id):
-#     # Do a 404 Error check
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("Question does not exist")
-#     # There is a get_object_or_404 shortcut to make it easier:
-#     question = get_object_or_404(Question, pk=question_id)
-#     context = {
-#         "question": question,
-#         "question_id": question_id,
-#     } 
-#     return render(request, "polls/detail.html", context)
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, "polls/results.html", {"question": question})
-
-
 # Class-based view for the index page.
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+# class IndexView(generic.ListView):
+#     template_name = "polls/index.html"
+#     context_object_name = "latest_question_list"
     
-    def get_queryset(self):
-        # Return the last five published questions
-        return Question.objects.order_by("-pub_date")[:5]
+#     def get_queryset(self):
+#         # Return the last five published questions
+#         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
-# Class-based view for the detail page.
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = "polls/detail.html"
+# # Class-based view for the detail page.
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = "polls/detail.html"
 
-# Class-based view for the results page.
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = "polls/results.html"
+# # Class-based view for the results page.
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = "polls/results.html"
+
+
+# These are the function-based views
+# This loads a template called polls/index.html and passes it a context.
+# The context is a dictionary mapping template variable names to Python objects.
+def index(request):
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # Return the last five published questions (not including those set to be published in the future).
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # Django provides a shortcut for rendering a template. Just use render() function instead of loader and HttpResponse
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    # Do a 404 Error check
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # There is a get_object_or_404 shortcut to make it easier:
+    # Edited to use the pub_date filter to check if the pub_date is in the past
+    # and not in a future date
+    question = get_object_or_404(Question, pub_date__lte=timezone.now(), pk=question_id)
+    context = {
+        "question": question,
+        "question_id": question_id,
+    } 
+    return render(request, "polls/detail.html", context)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     # Get the question object
@@ -77,9 +80,8 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
 
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        # The reverse() function is a shortcut to avoid hardcoding a URL in the view.
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button.
+        # The reverse() (a very bad name) function is a shortcut to avoid hardcoding a URL in the view.
         # It takes a view name and returns a URL matching that view.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
