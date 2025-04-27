b23aab0f34eae683c4aa439e08294ddda7cd95d1
Takinomichi <okonomi.nyc@gmail.com>
2025-04-28 00:23:54 +0900

    modified admin page and settings.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # extra = 3 means that 3 empty Choice objects will be displayed in the admin interface
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    #fields = ["pub_date", "question_text"]
+    # create fieldsets to group the fields in the admin interface
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ['pub_date']}),
+    ]
+    # This will display the choices next to the questions creation form
+    inlines = [ChoiceInline]
+    # Show a list of questions with their fields
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -11,6 +11,16 @@ class Question(models.Model):
     # returns the question text in a query
     def __str__(self):
         return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1)
 
     def was_published_recently(self):
         now = timezone.now()
